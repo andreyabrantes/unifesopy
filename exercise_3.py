c441fc51307491d5b0b4e78c3005f2ff04dc0ed7
Andrey Campos <59719418+andreyabrantes@users.noreply.github.com>
2024-08-21 20:57:52 -0300

    exercise_3.py

---
+++ exercise_3.py
@@ -0,0 +1,10 @@
+def main() -> None:
+    numero1 = int(input("Digite o primeiro número: "))
+    numero2 = int(input("Digite o segundo número:: "))
+
+    if numero2 != 0:
+        divisao = numero1 / numero2
+        print(divisao)
+
+if __name__ == "__main__":
+    main()
