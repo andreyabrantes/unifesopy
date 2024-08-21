9f447d95bc5c487a8380c93eedd8d5bb83e295c0
Andrey Campos <59719418+andreyabrantes@users.noreply.github.com>
2024-08-21 20:56:32 -0300

    exercise_1.py

--- exercise_1.py
+++ exercise_1.py
@@ -1,20 +1,10 @@
-# Escreva um código onde leia a sequência de números e mostre a soma
-
 def main() -> None:
-    soma: int = int(input("Digite a quantidade de números que deseja somar: "))
-    if soma < 0: 
-        print("Por favor, digite um número válido")
-    
-    else: 
-        resultado: int = 0 
-        i: int = 0
-        
-        while i < soma:
-            qtdnumero: int = int(input(f"Coloque o número {i+1}: "))
-            resultado = resultado + qtdnumero
-            i = i + 1
-        
-        print (f"A soma dos números é {resultado}")
-    
+  numero1: int = int(input("Digite o primeiro número: "))
+  numero2: int = int(input("Digite o segundo número: "))
+
+  soma: int = numero1 + numero2
+
+  print(f"A soma de {numero1} + {numero2} é igual a {soma}")
+
 if __name__ == "__main__":
-    main()
+    main()  
